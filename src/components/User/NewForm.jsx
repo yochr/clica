dcf01359011de98ee9f083b0c124ec8dcf787dca
yochr <9743635y@gmail.com>
2024-02-03 22:39:37 +0200

    meeting with date time picker

--- src/components/User/NewForm.jsx
+++ src/components/User/NewForm.jsx
@@ -1,36 +1,47 @@
-import {Button, TextField } from "@mui/material"
-import React, { useState, useEffect } from 'react';
+import {Button } from "@mui/material"
+import React, { useState } from 'react';
 import { observer } from "mobx-react";
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 import SaveChanges from "./SaveChanges‏ ";
-import SelectService from "./ServicesList";
+import BusinnessServices from '../../store/BusinnessServices';
+
 const NewForm =(observer(()=>{
-    const [date, setDate] = useState("0000-00-00");
 
+  const [date, setDate] = useState(dayjs('2024-02-03T00:00'));
+  const [chosenService, setService] = React.useState('');
+  const names = BusinnessServices.servicesList;
 
-return (
-<>
-<SelectService/>
-<TextField
-    id="outlined-basic"
-    label="תאריך"
-    type="date"
-    value={date}
-    variant="filled"
-    color="secondary"
-    onChange={(e) => setDate(e.target.value)}
-/>
+return (<>
+<FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+    <InputLabel>שם השרות</InputLabel>
+    <Select
+       value={chosenService} label="שם השרות" 
+       onChange={(event) => setService(event.target.value)}>
+       {names.map((name) => (<MenuItem key={name} value={name.name}> {name.name} </MenuItem> ))}
+    </Select>
+</FormControl>
 
+<LocalizationProvider dateAdapter={AdapterDayjs}>
+    <DemoContainer components={['DateTimePicker']}>
+      <DateTimePicker
+        label="בחר תאריך" value={date}
+        onChange={(newValue) => setDate(newValue)}/>
+    </DemoContainer>
+</LocalizationProvider> <br/> <br/>
 
-<br/>
-<br/>
 <Button
-    variant="contained"
-    color="secondary"
-    onClick={()=> SaveChanges(date)}>
-    אישור
+    variant="contained" color="secondary"
+    onClick={()=> SaveChanges(chosenService, date)}> אישור
 </Button>
-</>
-)}
-)
-)
+</>)
+}))
 export default NewForm
