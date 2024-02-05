a3ec9aa6e7a276ef05e56dbeb35034b6e207fa87
yochr <9743635y@gmail.com>
2024-02-05 11:05:39 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+  // setServices("meeting room","rent rooms for businness meeting","100$");
+  // setServices("science room","rent rooms Experiments","150$");
+  // setServices("Events Hall","rent rooms big events","180$");
+  // setServices("Events room","rent rooms small events","80$");
+import React, { useState } from 'react';
 import './App.css'
 import { getBusinessData, getServices } from './store/server.js';
-import Routs from './store/Routs.jsx';
 import UserHome from './components/User/UserHome.jsx';
+import AdminPage from './components/Login/AdminPage.jsx';
+import { Outlet, Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
 
-  getBusinessData();
-  getServices();
+const [currentPage, setCurrentPage] = useState('/');
+const handlePageChange = (page) => {
+  setCurrentPage(page);
+};
 
+ getBusinessData();
+ getServices();
   
   return (
-    <>
-      <UserHome/>
-    </>  
-  )
+    <Router>   
+      <main>
+      <Routes>
+          <Route path="/" element={<UserHome />} />
+          <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+        <Outlet />
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
